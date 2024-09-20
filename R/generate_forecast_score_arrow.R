ffca70cdb3e17234c440b275138307ef179a1a99
addelany <addelany@gmail.com>
2024-09-20 16:04:54 -0500

    make flarev3 updates

--- R/generate_forecast_score_arrow.R
+++ R/generate_forecast_score_arrow.R
@@ -19,10 +19,11 @@ generate_forecast_score_arrow <- function(targets_df,
     if(is.null(bucket) | is.null(endpoint)){
       stop("scoring function needs bucket and endpoint if use_s3=TRUE")
     }
-    #vars <- arrow_env_vars()
+    vars <- arrow_env_vars()
+
     output_directory <- arrow::s3_bucket(bucket = bucket,
                                          endpoint_override =  endpoint)
-    #unset_arrow_vars(vars)
+    on.exit(unset_arrow_vars(vars))
   }else{
     if(is.null(local_directory)){
       stop("scoring function needs local_directory if use_s3=FALSE")
@@ -44,4 +45,21 @@ generate_forecast_score_arrow <- function(targets_df,
 
   arrow::write_dataset(df, path = output_directory, partitioning = c("site_id","model_id","reference_date"))
 
+}
+
+
+arrow_env_vars <- function(){
+  user_region <- Sys.getenv("AWS_DEFAULT_REGION")
+  user_meta <- Sys.getenv("AWS_EC2_METADATA_DISABLED")
+  Sys.unsetenv("AWS_DEFAULT_REGION")
+  Sys.setenv(AWS_EC2_METADATA_DISABLED="TRUE")
+
+  list(user_region=user_region, user_meta = user_meta)
+}
+
+unset_arrow_vars <- function(vars) {
+  Sys.setenv("AWS_DEFAULT_REGION" = vars$user_region)
+  if (vars$user_meta != "") {
+    Sys.setenv(AWS_EC2_METADATA_DISABLED = vars$user_meta)
+  }
 }
