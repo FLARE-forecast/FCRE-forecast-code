fe8d308aa54ea337854165c50250f0142b235e71
addelany <addelany@gmail.com>
2025-04-24 14:48:29 -0500

    update endpoint

--- workflows/glm_flare_v3/xgboost_inflow.R
+++ workflows/glm_flare_v3/xgboost_inflow.R
@@ -36,16 +36,16 @@ message('done initializing....starting forecasts')
 #  print(i)
 
   #config$run_config$forecast_start_datetime <- as.Date(date_sample[i])
-  reference_datetime <- lubridate::as_datetime(config$run_config$forecast_start_datetime)
-  noaa_date <- reference_datetime - lubridate::days(1)
+reference_datetime <- lubridate::as_datetime(config$run_config$forecast_start_datetime)
+noaa_date <- reference_datetime - lubridate::days(1)
 
-  ## Run Forecast
-    inflow_forecast <- xg_combine_model_runs(site_id = site_identifier,
-                                           forecast_start_datetime = reference_datetime,
-                                           use_s3_inflow = config$run_config$use_s3,
-                                           inflow_bucket = config$s3$inflow_drivers$bucket,
-                                           inflow_endpoint = config$s3$inflow_drivers$endpoint,
-                                           inflow_local_directory = file.path(lake_directory, "drivers/"),
-                                           forecast_horizon = config$run_config$forecast_horizon,
-                                           inflow_model = config$flow$forecast_inflow_model)
+## Run Forecast
+inflow_forecast <- xg_combine_model_runs(site_id = site_identifier,
+                                       forecast_start_datetime = reference_datetime,
+                                       use_s3_inflow = config$run_config$use_s3,
+                                       inflow_bucket = config$s3$inflow_drivers$bucket,
+                                       inflow_endpoint = config$s3$inflow_drivers$endpoint,
+                                       inflow_local_directory = file.path(lake_directory, "drivers/"),
+                                       forecast_horizon = config$run_config$forecast_horizon,
+                                       inflow_model = config$flow$forecast_inflow_model)
 #}
